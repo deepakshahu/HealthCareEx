095b338f8eff44d6009c3c8f2df83b32fd79c4d7
deepakshahu <deepakshahu1996@gmail.com>
2022-04-30 14:55:04 +0530

    first commit

---
+++ src/test/java/in/nareshit/deepak/HealthCareExApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nareshit.deepak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
