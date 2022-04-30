1976f72c7ffa6f074e878837eb3f60a92b4dc346
deepakshahu <deepakshahu1996@gmail.com>
2022-04-30 15:02:13 +0530

    spec repo update

---
+++ src/main/java/in/nareshit/deepak/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package in.nareshit.deepak.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.nareshit.deepak.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+
+}
