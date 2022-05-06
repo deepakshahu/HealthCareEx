e94bb3e8400e57fd9a92012fdf68802cf83151ba
deepakshahu <deepakshahu1996@gmail.com>
2022-05-06 14:48:08 +0530

    RegisterForm and Validation

--- src/main/java/in/nareshit/deepak/controller/SpecializationController.java
+++ src/main/java/in/nareshit/deepak/controller/SpecializationController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import in.nareshit.deepak.entity.Specialization;
@@ -17,9 +19,28 @@ public class SpecializationController {
 	
 	@Autowired
 	private ISpecializationService service;
+	
+	/*
+	 * 1. Show register page
+	 */
+	@GetMapping("/register")
+	public String displayRegister() {
+		return "SpecializationRegister";
+	}
+	
+	/*
+	 * 2. On Submit form save data
+	 */
+	@PostMapping("/save")
+	public String saveForm(@ModelAttribute Specialization specialization, Model model) {
+		Long id = service.saveSpecialization(specialization);
+		String message = "Record ("+id+") is created ";
+		model.addAttribute("message", message);
+		return "SpecializationRegister";
+	}
 
 	/*
-	 *1. display all Specializations
+	 * 3. display all Specializations
 	 */
 	@GetMapping("/all")
 	public String viewAll(Model model) {
